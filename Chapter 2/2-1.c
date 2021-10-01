58aada8f1a91eaca25ddfed6b2c953bbe67802b0
username2077 <markengineer@163.com>
2021-10-01 17:47:11 +0800

    update files
    - remove Exercise folder
    - add Chapter 2\2-1.c

---
+++ Chapter 2/2-1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<limits.h>
+
+int main(){
+	printf("CHAR_BIT = %d\n", CHAR_BIT);
+	printf("CHAR_MAX = %d\n", CHAR_MAX);
+	printf("CHAR_MIN = %d\n", CHAR_MIN);
+	printf("SCHAR_MAX = %d\n", SCHAR_MAX);
+	printf("SCHAR_MIN = %d\n", SCHAR_MIN);
+	printf("INT_MAX = %d\n", INT_MAX);
+	printf("INT_MIN = %d\n", INT_MIN);
+	printf("UINT_MAX = %u\n", UINT_MAX);
+	printf("LONG_MAX = %d\n", LONG_MAX);
+	printf("LONG_MIN = %d\n", LONG_MIN);
+	printf("ULONG_MAX = %u\n", ULONG_MAX);
+	return 0;
+}
